a509e252130929096e254173f262fc6c692cd6c1
German <german.mtz@protonmail.com>
2021-12-09 22:11:54 -0600

    add navbar render from array of objects exercise

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,11 +5,15 @@ import './Navbar.css'
 const Navbar = () => {
   const [active, setActive] = useState('')
 
-  const items = ['Acerca', 'Alumnos', 'Empresas', 'Cursos', 'Login']
+  const items = [
+    { name: 'Acerca' },
+    { name: 'Alumnos' },
+    { name: 'Empresas' },
+    { name: 'Cursos' },
+    { name: 'Login' },
+  ]
 
-  // const ActiveContent = (active) => {active ==== name ?  : null}
-
-  const list = items.map((name) => {
+  const list = items.map(({ name }) => {
     return (
       <li
         key={name}
@@ -24,7 +28,6 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <ul className="nav-list">{list}</ul>
-      <div>{/* <h1>{ActiveContent}</h1> */}</div>
     </div>
   )
 }
