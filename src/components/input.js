c6449c4871f264692df86697be9c7e3db298ef19
German <german.mtz@protonmail.com>
2021-12-15 21:46:16 -0600

    add form exercise solution

--- src/components/input.js
+++ src/components/input.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Input = ({ id, label, value, setValue }) => {
+const Input = ({ type = 'text', id, label, value, setValue }) => {
   return (
     <div>
       <label htmlFor={id}>{label}:</label>
       <input
+        type={type}
         id={id}
         value={value}
         onChange={(event) => setValue(event.target.value)}
--- src/pages/UsersNew/index.js
+++ src/pages/UsersNew/index.js
@@ -7,6 +7,9 @@ import './UsersNew.css'
 export default function UsersNew() {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
+  const [gender, setGender] = useState('')
+  const [ocupation, setOcupation] = useState('')
+  const [birthdate, setBirthdate] = useState('')
 
   // const handleFirstName = (event) => setFirstName(event.target.value)
   // const handleLastName = (event) => setLastName(event.target.value)
@@ -16,16 +19,43 @@ export default function UsersNew() {
       <h1>Crea un usuario</h1>
       <form>
         <Input
+          min={1}
+          max={5}
+          type="text"
           id="firstName"
-          label="firstName"
+          label="First Name"
           value={firstName}
           setValue={setFirstName}
         />
         <Input
+          min={1}
+          max={20}
+          type="text"
           id="lastName"
-          label="lastName"
+          label="Last Name"
           value={lastName}
           setValue={setLastName}
+        />
+        <Input
+          type="text"
+          id="gender"
+          label="Gender"
+          value={gender}
+          setValue={setGender}
+        />
+        <Input
+          type="text"
+          id="ocupation"
+          label="Ocupation"
+          value={ocupation}
+          setValue={setOcupation}
+        />
+        <Input
+          type="date"
+          id="birthdate"
+          label="birthdate"
+          value={birthdate}
+          setValue={setBirthdate}
         />
       </form>
     </div>
