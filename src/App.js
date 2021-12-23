23d8f7db93b77f20aeeeb83170222cadbdf811c9
German <german.mtz@protonmail.com>
2021-12-22 21:09:27 -0600

    add React class component first example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import "./App.css";
+import './App.css'
+import { Component } from 'react'
 
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 
 // Components
-import AppContainer from "./components/AppContainer";
+import AppContainer from './components/AppContainer'
+import React from 'react'
 
-function App() {
-	return (
-		<AppContainer>
-			<Outlet />
-		</AppContainer>
-	);
+// function App() {
+//   return (
+//     <AppContainer>
+//       <Outlet />
+//     </AppContainer>
+//   )
+// }
+
+class App extends Component {
+  //   constructor(props) {
+  //     super(props)
+  //   }
+
+  render() {
+    return (
+      <AppContainer>
+        <Outlet />
+      </AppContainer>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,22 +1,19 @@
-import React from "react";
+import React from 'react'
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 export default function AppContainer({ children }) {
-	return (
-		<div className="main">
-			<nav className="main-nav">
-				<Link className="link" to="/">
-					Home
-				</Link>
-				<Link className="link" to="users">
-					Users
-				</Link>
-			</nav>
-			<div className="main-content">
-				Esto estará en todo lo que app contenga
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="main">
+      <nav className="main-nav">
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="users">
+          Users
+        </Link>
+      </nav>
+      <div className="main-content">{children}</div>
+    </div>
+  )
 }
